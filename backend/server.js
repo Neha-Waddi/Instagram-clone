5ab5c0a7d52db3188acdadc535250a44cc28460e
Neha-Waddi <waddineha.22.csm@anits.edu.in>
2025-04-01 23:28:46 +0530

    connected routes

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,20 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
 
+const userRoutes = require("./routes/userRoutes");
+const postRoutes = require("./routes/postRoutes");
+
+
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
